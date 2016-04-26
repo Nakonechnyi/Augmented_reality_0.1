4b1b87b2098cb62e2b011ad09ee9afbca9244fed
anton_nakonechnyi <anton.nak@gmail.com>
2016-04-26 12:41:39 +0300

    Initial commit. Spring Boot  - Started

---
+++ src/main/java/com/StartApi.java
@@ -0,0 +1,15 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @autor A_Nakonechnyi
+ * @date 25.04.2016.
+ */
+@SpringBootApplication
+public class StartApi {
+    public static void main(String[] args) {
+        SpringApplication.run(StartApi.class, args);
+    }
+}
---
+++ src/main/java/com/controller/GreetingController.java
@@ -0,0 +1,26 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.entity.Greeting;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * @autor A_Nakonechnyi
+ * @date 25.04.2016.
+ */
+@RestController
+public class GreetingController {
+
+        private static final String template = "Hello, %s!";
+        private final AtomicLong counter = new AtomicLong();
+
+        @RequestMapping("/greeting")
+        public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+            return new Greeting(counter.incrementAndGet(),
+                    String.format(template, name));
+        }
+
+}
